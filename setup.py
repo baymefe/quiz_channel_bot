aabc76cebe824fc2cc4497c7fa2285bcc6792d30
baymefe <mefeby22@gmail.com>
2023-01-03 18:45:44 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='quiz_channel_bot',
+    version='1.1',
+    packages=[''],
+    url='',
+    license='',
+    author='evetyeah',
+    author_email='mefeby22@gmail.com',
+    description='quiz video bot'
+)
